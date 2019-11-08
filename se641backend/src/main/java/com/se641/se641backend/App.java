ec87b4af39c33ea289ae1d4dc181de1fe0dde074
Zakaria <zknabdi@gmail.com>
2019-11-07 22:52:27 -0600

    SE 641 - Initial Set Up

---
+++ se641backend/src/main/java/com/se641/se641backend/App.java
@@ -0,0 +1,13 @@
+package com.se641.se641backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
