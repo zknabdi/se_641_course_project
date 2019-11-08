ad05c3188995f154f86765d8542653a28435d154
Zakaria <zknabdi@gmail.com>
2019-11-08 00:13:11 -0600

    Adding Spring Configuration and initial controller

---
+++ se614onlineshopping/src/main/java/com/se641/controller/AppController.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.se641.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author zknabi
+ *
+ */
+@Controller
+public class AppController {
+	
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("app"); //app.jsp
+		String message = "Hi, Test!";
+		mv.addObject("greeting", message);
+		
+		return mv;
+		
+		
+	}
+
+}
